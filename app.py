dff9abfec03a7fee73628097abcf59d9c4388f33
ahmed32-bl <amaroucheahmed32@gmail.com>
2025-03-24 07:43:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ def webhook():
     return "ok"
 
 # ============ تشغيل التطبيق ============
-if __name__ == '__main__':
-        port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
